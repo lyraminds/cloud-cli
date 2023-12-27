3b3b54d8cb27bf317219097f95ad3783d2bf949d
deploy <deploy@infospica.com>
2023-12-27 16:16:31 +0530

    updated installer

--- bin/install.sh
+++ bin/install.sh
@@ -1,11 +1,26 @@
 
 
 TAG=${1:-main}
+set -e
+
 echo "tag version ${TAG}"
-mkdir /opt
+mkdir -p /opt
 cd /opt
+
+if [ -d "cloud-cli" ]; then
+cd cloud-cli
+echo `cat doc/version.txt`
+echo "To install latest version delete the current folder
+      sudo rm -rf /opt/cloud-cli"
+else
+echo "cloud-cli not found..."
 git clone --depth 1 --branch ${TAG} https://github.com/lyraminds/cloud-cli.git cloud-cli
 
+if [ -f "/usr/bin/az-onboard" ]; then
 sudo rm /usr/bin/az-onboard
-
+fi
 sudo ln -s /opt/cloud-cli/az/onboard.sh /usr/bin/az-onboard
+
+fi
+
+
