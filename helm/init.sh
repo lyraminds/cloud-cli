c782fd73dc4505717737115a1ef66bd206221bd3
deploy <deploy@infospica.com>
2024-01-23 15:06:08 +0530

    signing issue

--- helm/init.sh
+++ helm/init.sh
@@ -1,6 +1,15 @@
 
 source bin/base.sh
 
+if [ $(installed "gpg") == "false" ]; then
+run-install "gpg"
+fi
+
+if [ ! -f "/usr/share/keyrings/hashicorp-archive-keyring.gpg" ]; then
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+fi
 
 if [ $(installed "helm") == "false" ]; then
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
